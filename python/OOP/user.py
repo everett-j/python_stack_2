74ba2f4892e68716c3283f678dbcf99e0c4d0c9d
Josh <jseverett16@gmail.com>
2019-08-05 16:58:18 -0700

    1700 080519

---
+++ python/OOP/user.py
@@ -0,0 +1,24 @@
+class User:
+    def __init__(self, username, email_address):
+        self.name = username
+        self.email = email_address
+        self.account_balance = 0
+
+josh = User("Josh Everett", "jseverett16@gmail.com")
+britt = User("Brittany Everett", "brittanyja2016@gmail.com")
+addison = User("Addison Everett", "munchkineverett@gmail.com")
+
+def make_deposit (self, amount)
+    self.account_balance += amount
+
+def make_withdrawl (self, amount)
+    self.account_balance -= amount
+
+def display_user_balance (self)
+    self.account_balance
+
+def transfer_money (self, other_user, amount)
+    self.account_balance += amount
+    other_user.account_balance -= amount
+    self.account_balance
+    other_user.account_balance
