21bb1440b7b41bd523ee226a20888f57c019b398
Josh <jseverett16@gmail.com>
2019-08-05 17:59:35 -0700

    1800 080519

--- python/OOP/user.py
+++ python/OOP/user.py
@@ -3,22 +3,40 @@ class User:
         self.name = username
         self.email = email_address
         self.account_balance = 0
+    
+    def make_deposit (self, amount)
+        self.account_balance += amount
+
+    def make_withdrawl (self, amount)
+        self.account_balance -= amount
+
+    def display_user_balance (self)
+        self.account_balance
+
+    def transfer_money (self, other_user, amount)
+        self.account_balance += amount
+        other_user.account_balance -= amount
+        self.account_balance
+        other_user.account_balance
 
 josh = User("Josh Everett", "jseverett16@gmail.com")
 britt = User("Brittany Everett", "brittanyja2016@gmail.com")
 addison = User("Addison Everett", "munchkineverett@gmail.com")
 
-def make_deposit (self, amount)
-    self.account_balance += amount
+class BankAccount:
+    def __init__(self)
+    self.account_balance = 0
+    
+    def deposit (self, amount)
+        self.account_balance += amount
 
-def make_withdrawl (self, amount)
-    self.account_balance -= amount
+    def withdrawl (self, amount)
+        self.account_balance -= amount
 
-def display_user_balance (self)
-    self.account_balance
+    def display_account_info (self)
+        print ("Balance: $" self.account_balance)  
 
-def transfer_money (self, other_user, amount)
-    self.account_balance += amount
-    other_user.account_balance -= amount
-    self.account_balance
-    other_user.account_balance
+    def yield_interest (self)
+        interest = 0
+        if account_balance > 0:
+            print(self.account_balance * interest)  
