9a25e4f2cc16867ea8044c25f68358327c704ac3
Josh <jseverett16@gmail.com>
2019-08-07 11:03:28 -0700

    1103 080719

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -3,9 +3,7 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')                           
 def hello_world():
-    # Instead of returning a string, 
-    # we'll return the result of the render_template method, passing in the name of our HTML file
-    return render_template('index.html') 
+    return render_template("index.html", phrase="hello", times=5)
     
 @app.route('/success')
 def success():
